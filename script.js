28b607e9e6c44670a1a9ae1020620fb3a0c4f9f7
Mateus Ferrão da Costa <mateusferraocosta2000@gmail.com>
2024-10-30 19:56:24 -0300

    inicio edicao

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 window.addEventListener("load", () => {
     imprimeDados();
+    restauraModal();
 })
+
+function restauraModal(){
+    let tituloModal = document.getElementById('exampleModalLabel');
+    let btnModal = document.getElementById('btnAdd');
+
+    inputTitulo.setAttribute("placeholder", "Título");
+    inputSubtitulo.setAttribute("placeholder", "Subtítulo");
+    inputDesc.setAttribute("placeholder", "Descrição");
+
+    tituloModal.innerHTML = `Novo Card`;
+    btnModal.innerHTML = `Adicionar`;
+    
+}
 
 function salvaDados(dados) {
     localStorage.setItem('cards', JSON.stringify(dados));
@@ -29,8 +43,8 @@ function imprimeDados() {
     for (let i = 0; i < objDados.cards.length; i++) {
         const estrelaPreenchida = objDados.cards[i].favorito ? "bi-star-fill" : "bi-star";
 
-        if(objDados.cards[i].favorito){
-            strFavHtml+=`<div class="card mx-auto my-3 border-2 border-success rounded" style="width: 25rem;">
+        if (objDados.cards[i].favorito) {
+            strFavHtml += `<div class="card mx-auto my-3 border-2 border-success rounded" style="width: 25rem;">
                     <div class="card-body bg-success">
                         <div class="d-flex justify-content-end">
                             <button type="button" class="btn btn-success btn mx-2 id="btnFav${i}" onclick="alteraEstrela(${i})"><i class="bi ${estrelaPreenchida}"></i></button>
@@ -39,13 +53,14 @@ function imprimeDados() {
                         <h6 class="card-subtitle mb-2 text-body-secondary">${objDados.cards[i].subtitulo}</h6>
                         <p class="card-text text-white">${objDados.cards[i].texto}</p>
                         <div class="d-flex justify-content-end">
-                            <button type="button" class="btn btn-success btn mx-2"><i
+                            <button type="button" class="btn btn-success btn mx-2" data-bs-toggle="modal"
+                data-bs-target="#exampleModal" onclick="editaCard(${i})"><i
                                     class="bi bi-pencil-fill"></i></button>
                             <button type="button" class="btn btn-success btn"><i class="bi bi-trash3-fill"></i></button>
                         </div>
                     </div>
                 </div>`
-        }else{
+        } else {
             strToHtml += `<div class="card mx-auto my-3 border-2 border-success rounded" style="width: 25rem;">
                     <div class="card-body bg-success">
                         <div class="d-flex justify-content-end">
@@ -55,14 +70,15 @@ function imprimeDados() {
                         <h6 class="card-subtitle mb-2 text-body-secondary">${objDados.cards[i].subtitulo}</h6>
                         <p class="card-text text-white">${objDados.cards[i].texto}</p>
                         <div class="d-flex justify-content-end">
-                            <button type="button" class="btn btn-success btn mx-2"><i
+                            <button type="button" class="btn btn-success btn mx-2" data-bs-toggle="modal"
+                data-bs-target="#exampleModal" onclick="editaCard(${i})"><i
                                     class="bi bi-pencil-fill"></i></button>
                             <button type="button" class="btn btn-success btn"><i class="bi bi-trash3-fill"></i></button>
                         </div>
                     </div>
                 </div>`
         }
-        
+
     }
 
     telaTo.innerHTML = strToHtml;
@@ -71,6 +87,13 @@ function imprimeDados() {
 
 function incluirCard() {
     let objDados = leDados();
+
+    inputTitulo.removeAttribute("placeholder","Novo título");
+    inputTitulo.setAttribute("placeholder","Título");
+    inputSubtitulo.removeAttribute("placeholder","Novo subtítulo");
+    inputSubtitulo.setAttribute("placeholder","Subtítulo");
+    inputDesc.removeAttribute("placeholder","Nova descrição");
+    inputDesc.setAttribute("placeholder","Descrição");
 
     let strTitulo = document.getElementById('inputTitulo').value;
     let strSubtitutulo = document.getElementById('inputSubtitulo').value;
@@ -89,12 +112,41 @@ function incluirCard() {
     imprimeDados();
 }
 
-function alteraEstrela(index){
+function alteraEstrela(index) {
     let objDados = leDados();
-    
+
     objDados.cards[index].favorito = !objDados.cards[index].favorito;
 
     salvaDados(objDados);
 
     imprimeDados();
 }
+
+/*function editaCard(index) {
+    let objDados = leDados();
+
+    let inputTitulo = document.getElementById('inputTitulo');
+    let inputSubtitulo = document.getElementById('inputSubtitulo');
+    let inputDesc = document.getElementById('inputDesc');
+    let tituloModal = document.getElementById('exampleModalLabel');
+    let btnModal = document.getElementById('btnAdd');
+
+    inputTitulo.removeAttribute("placeholder","Título");
+    inputTitulo.setAttribute("placeholder","Novo título");
+    inputSubtitulo.removeAttribute("placeholder","Subtítulo");
+    inputSubtitulo.setAttribute("placeholder","Novo subtítulo");
+    inputDesc.removeAttribute("placeholder","Descrição");
+    inputDesc.setAttribute("placeholder","Nova descrição");
+
+    tituloModal.innerHTML = `Editar Card`
+    btnModal.innerHTML = `Salvar`;
+
+    objDados.cards[index].titulo = inputTitulo.value || objDados.cards[index].titulo;
+    objDados.cards[index].subtitulo = inputSubtitulo.value || objDados.cards[index].subtitulo;
+    objDados.cards[index].texto = inputDesc.value || objDados.cards[index].texto;
+
+    salvaDados(objDados);
+
+    imprimeDados();
+
+}*/
