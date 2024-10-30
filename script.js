9b24e537a8c9a23d0160790d6acefe45b2f33829
Mateus Ferrão da Costa <mateusferraocosta2000@gmail.com>
2024-10-30 18:49:30 -0300

    funcao favoritar

--- script.js
+++ script.js
@@ -21,14 +21,19 @@ function leDados() {
 
 function imprimeDados() {
     let objDados = leDados();
-    let tela = document.getElementById('toDoList');
-    let strHtml = ``;
+    let telaTo = document.getElementById('toDoList');
+    let telaFav = document.getElementById('favoritos');
+    let strToHtml = ``;
+    let strFavHtml = ``;
 
     for (let i = 0; i < objDados.cards.length; i++) {
-        strHtml += `<div class="card mx-auto my-3 border-2 border-success rounded" style="width: 25rem;">
+        const estrelaPreenchida = objDados.cards[i].favorito ? "bi-star-fill" : "bi-star";
+
+        if(objDados.cards[i].favorito){
+            strFavHtml+=`<div class="card mx-auto my-3 border-2 border-success rounded" style="width: 25rem;">
                     <div class="card-body bg-success">
                         <div class="d-flex justify-content-end">
-                            <button type="button" class="btn btn-success btn mx-2"><i class="bi bi-star"></i></button>
+                            <button type="button" class="btn btn-success btn mx-2 id="btnFav${i}" onclick="alteraEstrela(${i})"><i class="bi ${estrelaPreenchida}"></i></button>
                         </div>
                         <h5 class="card-title text-white">${objDados.cards[i].titulo}</h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary">${objDados.cards[i].subtitulo}</h6>
@@ -40,14 +45,32 @@ function imprimeDados() {
                         </div>
                     </div>
                 </div>`
+        }else{
+            strToHtml += `<div class="card mx-auto my-3 border-2 border-success rounded" style="width: 25rem;">
+                    <div class="card-body bg-success">
+                        <div class="d-flex justify-content-end">
+                            <button type="button" class="btn btn-success btn mx-2 id="btnFav${i}" onclick="alteraEstrela(${i})"><i class="bi ${estrelaPreenchida}"></i></button>
+                        </div>
+                        <h5 class="card-title text-white">${objDados.cards[i].titulo}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">${objDados.cards[i].subtitulo}</h6>
+                        <p class="card-text text-white">${objDados.cards[i].texto}</p>
+                        <div class="d-flex justify-content-end">
+                            <button type="button" class="btn btn-success btn mx-2"><i
+                                    class="bi bi-pencil-fill"></i></button>
+                            <button type="button" class="btn btn-success btn"><i class="bi bi-trash3-fill"></i></button>
+                        </div>
+                    </div>
+                </div>`
+        }
+        
     }
 
-    tela.innerHTML = strHtml;
+    telaTo.innerHTML = strToHtml;
+    telaFav.innerHTML = strFavHtml;
 }
 
 function incluirCard() {
     let objDados = leDados();
-    let btnAdd = document.getElementById('btnAdd');
 
     let strTitulo = document.getElementById('inputTitulo').value;
     let strSubtitutulo = document.getElementById('inputSubtitulo').value;
@@ -66,3 +89,12 @@ function incluirCard() {
     imprimeDados();
 }
 
+function alteraEstrela(index){
+    let objDados = leDados();
+    
+    objDados.cards[index].favorito = !objDados.cards[index].favorito;
+
+    salvaDados(objDados);
+
+    imprimeDados();
+}
